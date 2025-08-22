07b9ff6310e436766858aa67f03a96c1e8fd5b88
Lala Ayoade Stephen <36624539+aylalah@users.noreply.github.com>
2025-08-22 22:43:10 +0100

    Update settings.py

--- app/swiftsuite/settings.py
+++ app/swiftsuite/settings.py
@@ -44,13 +44,13 @@ INSTALLED_APPS = [
     'django_cleanup.apps.CleanupConfig'
 ]
 
-# CORS_ALLOWED_ORIGINS = [
-#     "https://swiftsuite.app",
-#     "https://frontend-test.vps.swiftsuite.app",
-#     "http://localhost:5173"
-# ]
+CORS_ALLOWED_ORIGINS = [
+    "https://swiftsuite.app",
+    "https://frontend-test.vps.swiftsuite.app",
+    "http://localhost:5173"
+]
 
-CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOW_ALL_ORIGINS = True
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
