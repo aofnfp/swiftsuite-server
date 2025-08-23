414f9c3d1031e9630fac84c41e68036ba30daa15
Arise Damilare <arisedamilare5@gmail.com>
2025-08-23 11:29:42 +0100

    included router viewset urls and commented email ssl and tls

--- app/swiftsuite/settings.py
+++ app/swiftsuite/settings.py
@@ -121,8 +121,8 @@ DATABASES = {
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = config('EMAIL_HOST')
 EMAIL_PORT = config("EMAIL_PORT", cast=int)
-EMAIL_USE_SSL = config("EMAIL_USE_SSL", cast=bool) 
-EMAIL_USE_TLS = config("EMAIL_USE_TLS", cast=bool) 
+# EMAIL_USE_SSL = config("EMAIL_USE_SSL", cast=bool) 
+# EMAIL_USE_TLS = config("EMAIL_USE_TLS", cast=bool) 
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = config("DEFAULT_FROM_EMAIL")
