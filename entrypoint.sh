207bca7251764414813532584d9f2db6fbd12720
Arise Damilare <arisedamilare5@gmail.com>
2025-08-24 19:29:09 +0100

    updated the path

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "Running database migrations..."
-python app/manage.py migrate --noinput
+python manage.py migrate --noinput
 
 echo "Starting Gunicorn..."
 exec gunicorn --bind 0.0.0.0:8000 swiftsuite.wsgi:application --workers 4
