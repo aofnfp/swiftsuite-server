998fa0220bb9b9343f739f8341229cf4db0a6001
Yemi <sunday.opeyemi2018@gmail.com>
2025-09-03 11:51:45 +0100

    Some correction were made

--- app/orderApp/apps.py
+++ app/orderApp/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class OrderappConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "swiftsuite.orderApp"
+    name = "orderApp"
