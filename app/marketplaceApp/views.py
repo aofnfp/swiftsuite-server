87a79de2f248666107e6f0ebd56dda123a52cad7
Yemi <sunday.opeyemi2018@gmail.com>
2025-10-23 15:20:18 +0100

    some edit was made to view file

--- app/marketplaceApp/views.py
+++ app/marketplaceApp/views.py
@@ -408,7 +408,6 @@ class Ebay(APIView):
     # Create a connection to the eBay Trading API
     @api_view(['GET'])
     def get_product_to_list_detail(request, userid, market_name, prod_id):
-        print("testing for function activities")
         global product_id
         eb = Ebay()
         # refresh the refresh access_token
@@ -422,13 +421,13 @@ class Ebay(APIView):
             upc_code = product_details[0].get("upc")
 
             # Update the price of product with the calculated selling price
-            try:
-                start_price = eb.calculated_selling_price(enroll_id, product_details[0].get("total_product_cost"), product_details[0].get("id"), userid)
-                if type(start_price) != float:
-                    return Response(f"Failed to compute price, no valid data", status=status.HTTP_400_BAD_REQUEST)
-                product_details[0]["selling_price"] = start_price
-            except Exception as e:
-                return Response(f"Failed to fetch data: {e}", status=status.HTTP_400_BAD_REQUEST)
+            # try:
+            #     start_price = eb.calculated_selling_price(enroll_id, product_details[0].get("total_product_cost"), product_details[0].get("id"), userid)
+            #     if type(start_price) != float:
+            #         return Response(f"Failed to compute price, no valid data", status=status.HTTP_400_BAD_REQUEST)
+            #     product_details[0]["selling_price"] = start_price
+            # except Exception as e:
+            #     return Response(f"Failed to fetch data: {e}", status=status.HTTP_400_BAD_REQUEST)
             # Get the vendor's details of the product trying to list
             vendor_info[0]["vendor_location"] = list(Vendors.objects.all().filter(id=vendor_info[0].get("vendor_id")).values())
         except Exception as e:
