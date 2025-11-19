4131326030363626de8f21fb3116c6b12f1b4f3f
Yemi <sunday.opeyemi2018@gmail.com>
2025-11-19 09:45:39 +0100

    Error in listing was corrected

--- app/inventoryApp/views.py
+++ app/inventoryApp/views.py
@@ -380,7 +380,7 @@ class MarketInventory(APIView):
 class WooCommerceInventory(APIView):
     # Function to update product on woocommerce store
     @api_view(['PUT'])
-    def update_woocommerce_product(self, request, userid, market_name, inventory_id):
+    def update_woocommerce_product(request, userid, market_name, inventory_id):
         wooc = WooCommerce()
         try:
             enrollment = MarketplaceEnronment.objects.get(user_id=userid, marketplace_name=market_name)
