4d581299194d5b49ff7816f7138cedb58130abc7
Yemi <sunday.opeyemi2018@gmail.com>
2025-11-21 10:20:47 +0100

    update is made to order and inventory

--- app/inventoryApp/celery_schedule.py
+++ app/inventoryApp/celery_schedule.py
@@ -3,6 +3,6 @@ from celery.schedules import crontab
 APP_CELERY_BEAT_SCHEDULE = {
     "sync-ebay-every-half-hour": {
         "task": "inventoryApp.tasks.sync_ebay_inventory_task",
-        "schedule": crontab(minute=0, hour="*/30"),
+        "schedule": crontab(minute=0, hour="*/32"),
     }
 }
