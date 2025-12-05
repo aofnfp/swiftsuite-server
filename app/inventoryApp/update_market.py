e6d35f21f965cce3365b03b8bf01594b99a25024
Yemi <sunday.opeyemi2018@gmail.com>
2025-12-05 10:00:45 +0100

    new updates are added

--- app/inventoryApp/update_market.py
+++ app/inventoryApp/update_market.py
@@ -188,7 +188,7 @@ def update_ebay_price_quantity():
                     # Item exists, check if we need to update price or quantity
                     InventoryModel.objects.filter(Q(market_item_id=item.market_item_id) | Q(sku=item.sku)).update(start_price=selling_price, quantity=db_item.quantity, total_product_cost=total_product_cost)
                     # Update the product on Woocommerce
-                    response = update_woocommerce_product_from_background(item.market_item_id, vendor_name=item.vendor_name)
+                    response = update_woocommerce_product_from_background(item.market_item_id, selling_price, db_item.quantity, user.user_id)
                     item_to_save, created = UpdateLogModel.objects.update_or_create(user_id=item.user_id, inventory_id=item._id, defaults=dict(market_name="Woocommerce", vendor_name=item.vendor_name, updated_item=item.market_item_id, log_description=f"Updated price to {selling_price} and quantity to {db_item.quantity} from vendor {item.vendor_name}"))
                 
                 except Exception as e:
