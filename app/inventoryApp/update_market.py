8418106c32fa36b6d069fea0ec525d102ef56caf
Yemi <sunday.opeyemi2018@gmail.com>
2025-12-05 09:42:24 +0100

    new updates are added

--- app/inventoryApp/update_market.py
+++ app/inventoryApp/update_market.py
@@ -1,7 +1,8 @@
+import json
 import requests
 from rest_framework.decorators import api_view, permission_classes
 from ebaysdk.exception import ConnectionError
-from .models import InventoryModel
+from .models import InventoryModel, UpdateLogModel
 from marketplaceApp.views import Ebay
 from vendorEnrollment.models import CwrUpdate, FragrancexUpdate, LipseyUpdate, RsrUpdate, SsiUpdate, ZandersUpdate
 from marketplaceApp.models import MarketplaceEnronment
@@ -152,6 +153,8 @@ def update_ebay_price_quantity():
                     InventoryModel.objects.filter(Q(market_item_id=item.market_item_id) | Q(sku=item.sku)).update(start_price=selling_price, quantity=db_item.quantity, total_product_cost=total_product_cost)
                     # Update the product on Ebay
                     response = update_items_quantity_or_price_on_ebay(user.user_id, item.market_item_id, selling_price, db_item.quantity, user._id)
+                    item_to_save, created = UpdateLogModel.objects.update_or_create(user_id=item.user_id, inventory_id=item._id, defaults=dict(market_name="Ebay", vendor_name=item.vendor_name, updated_item=item.market_item_id, log_description=f"Updated price to {selling_price} and quantity to {db_item.quantity} from vendor {item.vendor_name}"))
+                    
 
                 except Exception as e:
                     print(f"Product fails to update price and quantity on ebay: {e}")
@@ -185,7 +188,8 @@ def update_ebay_price_quantity():
                     # Item exists, check if we need to update price or quantity
                     InventoryModel.objects.filter(Q(market_item_id=item.market_item_id) | Q(sku=item.sku)).update(start_price=selling_price, quantity=db_item.quantity, total_product_cost=total_product_cost)
                     # Update the product on Woocommerce
-                    response = update_woocommerce_product_from_background(item.market_item_id, selling_price, db_item.quantity, user.user_id)
+                    response = update_woocommerce_product_from_background(item.market_item_id, vendor_name=item.vendor_name)
+                    item_to_save, created = UpdateLogModel.objects.update_or_create(user_id=item.user_id, inventory_id=item._id, defaults=dict(market_name="Woocommerce", vendor_name=item.vendor_name, updated_item=item.market_item_id, log_description=f"Updated price to {selling_price} and quantity to {db_item.quantity} from vendor {item.vendor_name}"))
                 
                 except Exception as e:
                     print(f"Product fails to update price and quantity on Woocommerce: {e}")
