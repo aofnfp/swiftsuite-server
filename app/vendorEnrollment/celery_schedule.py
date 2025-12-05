7b8f58a5fb523786f37ff5acde65c1597681898e
Arise Damilare <arisedamilare5@gmail.com>
2025-12-05 12:24:29 +0100

    Add queue specification to shared tasks and enhance GeneralProductTable model

--- app/vendorEnrollment/celery_schedule.py
+++ app/vendorEnrollment/celery_schedule.py
@@ -3,6 +3,6 @@ from celery.schedules import crontab
 APP_CELERY_BEAT_SCHEDULE = {
     "update_all_enrollments": {
         "task": "vendorEnrollment.tasks.update_all_enrollments",
-        "schedule": crontab(hour=2, minute=0),  
+        "schedule": crontab(hour=2, minute=0),  # Every day at 2:00 AM
     }
 }
