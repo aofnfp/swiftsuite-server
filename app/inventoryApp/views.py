23cfc4b98d1e1b462456471a1fb423a00b3b6566
Yemi <sunday.opeyemi2018@gmail.com>
2025-12-20 22:01:50 +0100

    added search query to inventoryApp

--- app/inventoryApp/views.py
+++ app/inventoryApp/views.py
@@ -240,26 +240,27 @@ class General_operations:
             if serializer.is_valid():
                 serializer_data = serializer.validated_data
                 search_query = serializer_data['search_query']
-                
-                inventory_listing = InventoryModel.objects.all().filter(Q(user_id=userid) &
-                    Q(title__icontains=search_query) |
-                    Q(sku__icontains=search_query) |
-                    Q(upc__icontains=search_query) |
-                    Q(market_item_id__icontains=search_query) |
-                    Q(vendor_name__icontains=search_query) |
-                    Q(market_name__icontains=search_query)).values().order_by('id').reverse()
-            
-                page = request.GET.get('page', int(page_number))
-                paginator = Paginator(inventory_listing, int(num_per_page))
-                try:
-                    inventory_objects = paginator.page(page)
-                except PageNotAnInteger:
-                    inventory_objects = paginator.page(1)
-                except EmptyPage:
-                    inventory_objects = paginator.page(paginator.num_pages)
-                # Get enrollment details of the user too
-                enrollment = MarketplaceEnronment.objects.filter(user_id=userid).values()
-                return JsonResponse({"Total_count":len(inventory_listing), "Total_pages":paginator.num_pages, "Inventory_items":list(inventory_objects), "enrollment_detail":list(enrollment)}, safe=False, status=status.HTTP_200_OK)
+            else:
+                return Response(f"Search query not provided.", status=status.HTTP_400_BAD_REQUEST) 
+            inventory_listing = InventoryModel.objects.all().filter(user_id=userid).filter(
+                Q(title__icontains=search_query) |
+                Q(sku__icontains=search_query) |
+                Q(upc__icontains=search_query) |
+                Q(market_item_id__icontains=search_query) |
+                Q(vendor_name__icontains=search_query) |
+                Q(market_name__icontains=search_query)).values().order_by('id').reverse()
+        
+            page = request.GET.get('page', int(page_number))
+            paginator = Paginator(inventory_listing, int(num_per_page))
+            try:
+                inventory_objects = paginator.page(page)
+            except PageNotAnInteger:
+                inventory_objects = paginator.page(1)
+            except EmptyPage:
+                inventory_objects = paginator.page(paginator.num_pages)
+            # Get enrollment details of the user too
+            enrollment = MarketplaceEnronment.objects.filter(user_id=userid).values()
+            return JsonResponse({"Total_count":len(inventory_listing), "Total_pages":paginator.num_pages, "Inventory_items":list(inventory_objects), "enrollment_detail":list(enrollment)}, safe=False, status=status.HTTP_200_OK)
             
         except Exception as e:
             return Response(f"Failed to get items. {e}", status=status.HTTP_400_BAD_REQUEST)
