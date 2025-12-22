2376ef475ff8b980cdb70c3a163f4cea1d6ab54a
Arise Damilare <arisedamilare5@gmail.com>
2025-12-22 23:41:04 +0100

    Fix: Update celery schedule syntax for tasks in inventoryApp and orderApp

--- app/inventoryApp/celery_schedule.py
+++ app/inventoryApp/celery_schedule.py
@@ -3,14 +3,14 @@ from celery.schedules import crontab
 APP_CELERY_BEAT_SCHEDULE = {
     "sync-ebay-every-half-hour": {
         "task": "inventoryApp.tasks.sync_ebay_inventory_task",
-        "schedule": crontab(minute=0, hour="*/30"),
+        "schedule": crontab(minute="*/30"),
     },
     "update-ebay-price-quantity-30min": {
         "task": "inventoryApp.tasks.update_ebay_price_quantity_inventory_task",
-        "schedule": crontab(minute=0, hour="*/30"),
+        "schedule": crontab(minute="*/30"),
     },
     "check-ebay-item-ended-30min": {
         "task": "inventoryApp.tasks.check_ebay_item_ended_task",
-        "schedule": crontab(minute=0, hour="*/30"),
+        "schedule": crontab(minute="*/30"),
     }
 }
