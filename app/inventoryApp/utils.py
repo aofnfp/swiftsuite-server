592eea4e34ce4f7ded8726d05b6e4728a95db0b4
Yemi <sunday.opeyemi2018@gmail.com>
2026-01-01 16:40:03 +0100

    New update made

--- app/inventoryApp/utils.py
+++ app/inventoryApp/utils.py
@@ -290,9 +290,7 @@ def sync_ebay_items_with_local():
                                 item_product = item_product[0]
                             except:
                                 item_product = Generalproducttable.objects.create(user_id=user.user_id, sku=db_items.sku, upc=db_items.upc, mpn=db_items.mpn, active=True, total_product_cost=db_items.total_price, map=db_items.map, enrollment_id=db_items.enrollment_id, product_id=db_items.product_id, quantity=db_items.quantity, price=db_items.price, vendor_name=db_items.vendor.name)
-                            
-                            if isinstance(item_product, list) or isinstance(item_product, tuple):
-                                item_product = item_product[0]
+                    
                             # insert mapped item into inventory
                             inentory, created = InventoryModel.objects.update_or_create(market_item_id=item.get("id"), user_id=user.user_id, defaults={"map_status": True, "product_id": item_product.id, "total_product_cost": db_items.total_price, "price": db_items.price, "vendor_name": db_items.vendor.name, "market_item_url": item.get("permalink")})
                             # Update the VendorUpdate table to set listed_market to true
