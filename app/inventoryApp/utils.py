9b877f57d1d8e9a1d4786806128fa11216433c88
Yemi <sunday.opeyemi2018@gmail.com>
2026-01-09 23:34:06 +0100

    some corrections were made

--- app/inventoryApp/utils.py
+++ app/inventoryApp/utils.py
@@ -393,11 +393,9 @@ def map_marketplace_items_to_vendor():
         # Get list of vendors registered by the user
         enrollment = Enrollment.objects.filter(user_id=user.user_id)
         vendor_list = [(vendor.vendor.name.capitalize(), vendor.id) for vendor in enrollment]
-        print(f"Vendor list for user {user.user_id}: {vendor_list}")
         
         # fetch all items from inventory for the user
         all_marketplace_items = InventoryModel.objects.filter(user_id=user.user_id, manual_map=False)
-        print(f"Total marketplace items to map for user {user.user_id}: {all_marketplace_items.count()}")
         for item in all_marketplace_items:
             db_items = None
             # Find the product in vendor update tables
@@ -436,5 +434,4 @@ def map_marketplace_items_to_vendor():
                     
                 except Exception as e:
                     print(f"Mapping Product processing failed with error: {e}")
-            else:
-                print(f"No matching vendor product found for inventory item SKU: {item.sku}, UPC: {item.upc}, MPN: {item.mpn}")
+           
