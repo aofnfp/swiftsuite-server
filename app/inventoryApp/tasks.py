0f683d565b601020bad3cfb5fc8aec913268ed3b
Yemi <sunday.opeyemi2018@gmail.com>
2026-01-09 23:22:47 +0100

    some corrections were made

--- app/inventoryApp/tasks.py
+++ app/inventoryApp/tasks.py
@@ -55,7 +55,7 @@ def check_product_ended_status_task(self):
 
 
 # LOCK_KEY4 = "map_marketplace_items_to_vendor_task_lock"
-# @shared_task(queue='default')
+@shared_task(queue='default')
 def map_marketplace_items_to_vendor_task():
     # if not cache.add(LOCK_KEY4, "1", timeout=7200):
     #     logger.info("map_marketplace_items_to_vendor_task skipped: already running")
