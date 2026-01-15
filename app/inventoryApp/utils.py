0e94717bc9c408e2e0111dd379bf683a3574d6c2
Yemi <sunday.opeyemi2018@gmail.com>
2026-01-15 09:18:21 +0100

    New updates were made

--- app/inventoryApp/utils.py
+++ app/inventoryApp/utils.py
@@ -398,6 +398,7 @@ def map_marketplace_items_to_vendor():
         # fetch all items from inventory for the user
         all_marketplace_items = InventoryModel.objects.filter(user_id=user.user_id, manual_map=False, map_status=False)
         for item in all_marketplace_items:
+            logger.info(f"Mapping item {item.sku} for user {user.user_id}")
             db_items = None
             for vendor_name, enrolled_id in vendor_list:
                 try:
