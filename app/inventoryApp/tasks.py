c8c0416b99d14108226397c15f453d4819628f45
Yemi <sunday.opeyemi2018@gmail.com>
2026-01-24 21:39:24 +0100

    New updates were made

--- app/inventoryApp/tasks.py
+++ app/inventoryApp/tasks.py
@@ -165,62 +165,61 @@ def download_item_update_market_price_quantity(self, months_back=12):
                     all_ebay_items.extend(window_items)
                     end = start
 
+                    # If fetching items failed due to invalid token, try refreshing token once and fetch again
+                    if all_ebay_items == None:
+                        logger.info(f"Ebay inventory download failed with error: {all_ebay_items}")
+                        continue
+                    # Construct a list of ebay items with relevant details
+                    for item in all_ebay_items:
+                        all_ebay_items.append({"ebay_item_id":item[0], "ebay_sku":item[1], 'Title':item[2], "ebay_price":item[3], "ebay_quantity":item[4], 'ListingDuration':item[5], 'ListingType':item[6], 'PictureDetails':item[7], 'ShippingProfileID':item[8], 'ShippingProfileName':item[9], 'ReturnProfileID':item[10], 'ReturnProfileName':item[11], 'PaymentProfileID':item[12], 'PaymentProfileName':item[13], 'market_item_url':item[14]})
+                    logger.info(f"Ebay inventory download fetched {len(all_ebay_items)} items for user {user.user_id}")
+                    # Loop through each item and update or insert into InventoryModel
+                    for item in all_ebay_items:                         
+                        try:
+                            # If item already exists, skip to next item
+                            existing_item = InventoryModel.objects.get(user_id=user.user_id, market_item_id=item.get("ebay_item_id"))
+                            # Update the market url on inventory
+                            InventoryModel.objects.filter(user_id=user.user_id, market_item_id=item.get("ebay_item_id")).update(market_item_url=item.get("market_item_url"), last_updated=timezone.now())
+                            if existing_item.market_item_id == "" or existing_item.vendor_name == "Not Found":
+                                continue
+                            # Update the price and quantity of product on Ebay
+                            if existing_item.start_price != item.get("ebay_price") or existing_item.quantity != item.get("ebay_quantity"):
+                                response = update_items_quantity_or_price_on_ebay(user.user_id, item.get("ebay_item_id"), existing_item.start_price, existing_item.quantity, user._id)
+                                item_to_save, created = UpdateLogModel.objects.update_or_create(user_id=user.user_id, inventory_id=existing_item.id, defaults=dict(market_name="Ebay", vendor_name=existing_item.vendor_name, updated_item=item.sku, log_description=f"Updated price to {existing_item.start_price} and quantity to {existing_item.quantity} from vendor {existing_item.vendor_name}"))
+
+                        except:
+                            try:
+                                # Get product details from eBay
+                                product_details = get_item_details(user._id, item.get("ebay_item_id"))
+                                if product_details == None:
+                                    logger.info(f"Ebay get product details failed for item id {item.get('ebay_item_id')} with error: {product_details}")
+                                    continue
+                                else:
+                                    logger.info(f"Ebay get product details succeeded for item id {item.get('ebay_item_id')}")
+                                    # Get the upc and mpn if the main mpn field does not exist
+                                    for specific in product_details.get("localizedAspects"):
+                                        ebay_upc = specific.get("value") if specific.get("name") == "UPC" else ""
+                                        ebay_mpn = specific.get("value") if specific.get("name") == "MPN" else product_details.get("mpn") 
+
+                                    # Put all the custom fields in the dictionary
+                                    custom_fields = {}
+                                    for object in product_details.get("localizedAspects"):
+                                        custom_fields[object.get("name")] = object.get("value")
+                                        
+                                    inentory, created = InventoryModel.objects.update_or_create(user_id=user.user_id, market_item_id=item.get("ebay_item_id"), defaults={"title": item.get("Title"),"description": json.dumps(product_details.get("shortDescription")), "location": product_details.get("itemLocation")["country"], "category_id": product_details.get("categoryId"), "category": product_details.get("categoryPath"), "sku": item.get("ebay_sku"), "upc": ebay_upc, "mpn": ebay_mpn, "start_price": product_details.get("price")["value"], "price": product_details.get("price")["value"], "cost": product_details.get("price")["value"], "picture_detail": product_details.get("image")["imageUrl"], "thumbnailImage": product_details.get("additionalImages"), "postal_code": product_details.get("itemLocation")["postalCode"], "city": product_details.get("itemLocation")["city"], "country": product_details.get("itemLocation")["country"], "quantity": item.get("ebay_quantity"), "return_profileID": item.get("ReturnProfileID"), "return_profileName": item.get("ReturnProfileName"), "payment_profileID": item.get("PaymentProfileID"), "payment_profileName": item.get("PaymentProfileName"), "shipping_profileID": item.get("ShippingProfileID"), "shipping_profileName": item.get("ShippingProfileName"), "bestOfferEnabled": True, "listingType": item.get("ListingType"), "item_specific_fields": custom_fields, "market_logos": product_details.get("listingMarketplaceId"), "date_created": product_details.get("itemCreationDate").split("T")[0], "active": True, "vendor_name": "Not Found", "map_status": False, "market_name": "Ebay", "fixed_percentage_markup": user.fixed_percentage_markup, "fixed_markup": user.fixed_markup, "profit_margin": user.profit_margin, "min_profit_mergin": user.min_profit_mergin, "charity_id": user.charity_id, "enable_charity": user.enable_charity, "market_item_url": item.get("market_item_url")})
+                            except Exception as e:
+                                logger.info(f"Ebay Product failed to insert into inventory {e}")
+                                continue
+                    
                     # Optional: progress reporting
                     self.update_state(
                         state="PROGRESS",
                         meta={"items_fetched": len(all_ebay_items)}
                     )
-
+       
             except Exception as e:
                 logger.info(f"Ebay inventory download failed with error: {e}")
                 continue
-
-            # If fetching items failed due to invalid token, try refreshing token once and fetch again
-            if all_ebay_items == None:
-                logger.info(f"Ebay inventory download failed with error: {all_ebay_items}")
-                continue
-            # Construct a list of ebay items with relevant details
-            for item in all_ebay_items:
-                all_ebay_items.append({"ebay_item_id":item[0], "ebay_sku":item[1], 'Title':item[2], "ebay_price":item[3], "ebay_quantity":item[4], 'ListingDuration':item[5], 'ListingType':item[6], 'PictureDetails':item[7], 'ShippingProfileID':item[8], 'ShippingProfileName':item[9], 'ReturnProfileID':item[10], 'ReturnProfileName':item[11], 'PaymentProfileID':item[12], 'PaymentProfileName':item[13], 'market_item_url':item[14]})
-            logger.info(f"Ebay inventory download fetched {len(all_ebay_items)} items for user {user.user_id}")
-            # Loop through each item and update or insert into InventoryModel
-            for item in all_ebay_items:                         
-                try:
-                    # If item already exists, skip to next item
-                    existing_item = InventoryModel.objects.get(user_id=user.user_id, market_item_id=item.get("ebay_item_id"))
-                    # Update the market url on inventory
-                    InventoryModel.objects.filter(user_id=user.user_id, market_item_id=item.get("ebay_item_id")).update(market_item_url=item.get("market_item_url"), last_updated=timezone.now())
-                    if existing_item.market_item_id == "" or existing_item.vendor_name == "Not Found":
-                        continue
-                    # Update the price and quantity of product on Ebay
-                    if existing_item.start_price != item.get("ebay_price") or existing_item.quantity != item.get("ebay_quantity"):
-                        response = update_items_quantity_or_price_on_ebay(user.user_id, item.get("ebay_item_id"), existing_item.start_price, existing_item.quantity, user._id)
-                        item_to_save, created = UpdateLogModel.objects.update_or_create(user_id=user.user_id, inventory_id=existing_item.id, defaults=dict(market_name="Ebay", vendor_name=existing_item.vendor_name, updated_item=item.sku, log_description=f"Updated price to {existing_item.start_price} and quantity to {existing_item.quantity} from vendor {existing_item.vendor_name}"))
-
-                except:
-                    try:
-                        # Get product details from eBay
-                        product_details = get_item_details(user._id, item.get("ebay_item_id"))
-                        if product_details == None:
-                            logger.info(f"Ebay get product details failed for item id {item.get('ebay_item_id')} with error: {product_details}")
-                            continue
-                        else:
-                            logger.info(f"Ebay get product details succeeded for item id {item.get('ebay_item_id')}")
-                            # Get the upc and mpn if the main mpn field does not exist
-                            for specific in product_details.get("localizedAspects"):
-                                ebay_upc = specific.get("value") if specific.get("name") == "UPC" else ""
-                                ebay_mpn = specific.get("value") if specific.get("name") == "MPN" else product_details.get("mpn") 
-
-                            # Put all the custom fields in the dictionary
-                            custom_fields = {}
-                            for object in product_details.get("localizedAspects"):
-                                custom_fields[object.get("name")] = object.get("value")
-                                
-                            inentory, created = InventoryModel.objects.update_or_create(user_id=user.user_id, market_item_id=item.get("ebay_item_id"), defaults={"title": item.get("Title"),"description": json.dumps(product_details.get("shortDescription")), "location": product_details.get("itemLocation")["country"], "category_id": product_details.get("categoryId"), "category": product_details.get("categoryPath"), "sku": item.get("ebay_sku"), "upc": ebay_upc, "mpn": ebay_mpn, "start_price": product_details.get("price")["value"], "price": product_details.get("price")["value"], "cost": product_details.get("price")["value"], "picture_detail": product_details.get("image")["imageUrl"], "thumbnailImage": product_details.get("additionalImages"), "postal_code": product_details.get("itemLocation")["postalCode"], "city": product_details.get("itemLocation")["city"], "country": product_details.get("itemLocation")["country"], "quantity": item.get("ebay_quantity"), "return_profileID": item.get("ReturnProfileID"), "return_profileName": item.get("ReturnProfileName"), "payment_profileID": item.get("PaymentProfileID"), "payment_profileName": item.get("PaymentProfileName"), "shipping_profileID": item.get("ShippingProfileID"), "shipping_profileName": item.get("ShippingProfileName"), "bestOfferEnabled": True, "listingType": item.get("ListingType"), "item_specific_fields": custom_fields, "market_logos": product_details.get("listingMarketplaceId"), "date_created": product_details.get("itemCreationDate").split("T")[0], "active": True, "vendor_name": "Not Found", "map_status": False, "market_name": "Ebay", "fixed_percentage_markup": user.fixed_percentage_markup, "fixed_markup": user.fixed_markup, "profit_margin": user.profit_margin, "min_profit_mergin": user.min_profit_mergin, "charity_id": user.charity_id, "enable_charity": user.enable_charity, "market_item_url": item.get("market_item_url")})
-                    except Exception as e:
-                        logger.info(f"Ebay Product failed to insert into inventory {e}")
-                        continue
-
 
 # @shared_task(
 #     bind=True,
