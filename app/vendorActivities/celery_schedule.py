3b8cdccb3e2f019a90c382e230dbfed67061a595
Arise Damilare <arisedamilare5@gmail.com>
2026-01-28 13:03:38 +0100

    Refactor: vendor activities schedule to every 6hrs

--- app/vendorActivities/celery_schedule.py
+++ app/vendorActivities/celery_schedule.py
@@ -3,7 +3,7 @@ from celery.schedules import crontab
 APP_CELERY_BEAT_SCHEDULE = {
     "reload_all_vendors": {
         "task": "vendorActivities.tasks.reload_all_vendors",
-        "schedule": crontab(hour=2, minute=0),  # run daily at 2 AM
+        "schedule": crontab(minute=0, hour='*/6'),  # Every 6 hours
     }
 }
 
