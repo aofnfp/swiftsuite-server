67173daea7ba95c41dbc7d2f0f2529fa66f7bb13
Yemi <sunday.opeyemi2018@gmail.com>
2026-01-29 14:29:53 +0100

    New codes added

--- app/orderApp/urls.py
+++ app/orderApp/urls.py
@@ -1,6 +1,5 @@
 from .views import OrderEbay as eb_view
 from django.urls import path
-from .utils import sync_ebay_order_with_local
 from .fragranceX_order import place_order_fragrancex, getTracking_fragranceX
 from .rsr_order import place_order_rsr
 
@@ -9,10 +8,8 @@ urlpatterns = [
     path('get_ebay_orders/<int:userid>/<int:page_number>/<int:num_per_page>/', eb_view.get_product_ordered, name='get_ebay_orders'),
     path('get_ordered_item_details/<int:userid>/<str:market_name>/<str:ebayorderid>/', eb_view.get_ordered_item_details, name='get_ordered_item_details'),
     path('cancel_ordered_item/<int:userid>/<str:market_name>/<str:ebayorderid>/', eb_view.cancel_order_from_ebay, name='cancel_ordered_item'),
-    path('sync_ordered_item/', sync_ebay_order_with_local, name='sync_ordered_item'),
-    
     path('place_order_fragrancex/<str:market_name>/<str:orderid>/', place_order_fragrancex, name='place_order_fragrancex'),
     path('get_tracking_fragranceX/<str:orderId>/', getTracking_fragranceX, name='get_tracking_fragranceX'),
-    
     path('place_order_rsr/<str:market_name>/<str:orderid>/', place_order_rsr, name='place_order_rsr'),
+    # path('sync_ordered_item/', sync_ebay_order_with_local, name='sync_ordered_item'),
 ]
