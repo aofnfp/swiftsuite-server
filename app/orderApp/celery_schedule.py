62c1da92b906e06072dd4833d41b778a88272673
Arise Damilare <arisedamilare5@gmail.com>
2026-02-18 14:39:59 +0100

    Refactored: scheduled task

--- app/orderApp/celery_schedule.py
+++ app/orderApp/celery_schedule.py
@@ -7,11 +7,11 @@ APP_CELERY_BEAT_SCHEDULE = {
     },
     "process_vendor_orders": {
         "task": "orderApp.tasks.process_vendor_orders",
-        "schedule": crontab(minute="*/50"),
+        "schedule": crontab(minute="*/5"),
     },
     "check_vendor_orders": {
         "task": "orderApp.tasks.check_vendor_order_status",
-        "schedule": crontab(minute="*/50"),
+        "schedule": crontab(minute="*/5"),
     }
 }
 
