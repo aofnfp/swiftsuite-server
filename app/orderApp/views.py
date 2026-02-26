0bc1712145ca68e40ba394b81a17c895cafeea10
Arise Damilare <arisedamilare5@gmail.com>
2026-02-26 11:49:36 +0100

    Refactor: Track order

--- app/orderApp/views.py
+++ app/orderApp/views.py
@@ -448,7 +448,10 @@ class TrackOrderView(APIView):
                 status=status.HTTP_400_BAD_REQUEST
             )
 
-        elif vendor_order.vendor.lower() == "fragrancex":
+        elif vendor_order.vendor.lower() == "fragrancex" or vendor_order.enrollment.vendor.name.lower() == "fragrancex":
+            if not vendor_order.vendor:
+                vendor_order.vendor = 'fragrancex'  
+                vendor_order.save()
             
             if vendor_order.tracking_number and vendor_order.carrier:
                 return JsonResponse(
