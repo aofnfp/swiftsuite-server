37d4c08b5fdee3eee632a5e0cc494a9ed4e385d1
Yemi <sunday.opeyemi2018@gmail.com>
2026-03-03 14:00:54 +0100

    A new update were made

--- app/inventoryApp/utils.py
+++ app/inventoryApp/utils.py
@@ -381,12 +381,12 @@ def download_item_update_market_price_quantity():
 
                         response = update_items_quantity_or_price_on_ebay(user.user_id, item.get("ebay_item_id"), existing_item.start_price, item.get("ebay_quantity"), user._id)
                         if "Success" in response:
-                            item_to_save, created = MarketPlaceUpdateLog.objects.update_or_create(user_id=user.user_id, inventory_id=existing_item.id, defaults=dict(market_name="Ebay", vendor_name=existing_item.vendor_name, updated_sku=item.get("ebay_sku"), log_description=f"Updated price to {existing_item.start_price} and quantity to {item.get('ebay_quantity')} from vendor {existing_item.vendor_name}"))
-                            InventoryModel.objects.filter(user_id=user.user_id, inventory_id=existing_item.id).update(market_item_url=item.get("market_item_url"), last_updated=timezone.now())
+                            item_to_save, created = MarketPlaceUpdateLog.objects.update_or_create(user_id=user.user_id, id=existing_item.id, defaults=dict(market_name="Ebay", vendor_name=existing_item.vendor_name, updated_sku=item.get("ebay_sku"), log_description=f"Updated price to {existing_item.start_price} and quantity to {item.get('ebay_quantity')} from vendor {existing_item.vendor_name}"))
+                            InventoryModel.objects.filter(user_id=user.user_id, id=existing_item.id).update(market_item_url=item.get("market_item_url"), last_updated=timezone.now())
                         else:
                             logger.info(f"Failed to update price and quantity on eBay item {item.get('ebay_item_id')} with response: {response}")
                     else:
-                        InventoryModel.objects.filter(user_id=user.user_id, inventory_id=existing_item.id).update(market_item_url=item.get("market_item_url"))
+                        InventoryModel.objects.filter(user_id=user.user_id, id=existing_item.id).update(market_item_url=item.get("market_item_url"))
 
                 except Exception as e:
                     logger.info(f"Failed to update existing item on ebay with error: {e}")
@@ -425,13 +425,13 @@ def download_item_update_market_price_quantity():
                     if existing_item.start_price != item.get("price") or existing_item.quantity != item.get("stock_quantity"):
                         response = update_woocommerce_product_from_background(item.get("id"), existing_item.start_price, existing_item.quantity, user.user_id)
                         if response == "Success":
-                            item_to_save, created = MarketPlaceUpdateLog.objects.update_or_create(user_id=user.user_id, inventory_id=existing_item.id, defaults=dict(market_name="Woocommerce", vendor_name=existing_item.vendor_name, updated_sku=item.get('sku'), log_description=f"Updated price to {existing_item.start_price} and quantity to {existing_item.quantity} from vendor {existing_item.vendor_name}"))
+                            item_to_save, created = MarketPlaceUpdateLog.objects.update_or_create(user_id=user.user_id, id=existing_item.id, defaults=dict(market_name="Woocommerce", vendor_name=existing_item.vendor_name, updated_sku=item.get('sku'), log_description=f"Updated price to {existing_item.start_price} and quantity to {existing_item.quantity} from vendor {existing_item.vendor_name}"))
                             # Update the market url on inventory
-                            InventoryModel.objects.filter(user_id=user.user_id, inventory_id=existing_item.id).update(market_item_url=item.get("permalink"))
+                            InventoryModel.objects.filter(user_id=user.user_id, id=existing_item.id).update(market_item_url=item.get("permalink"))
                         else:
                             logger.info(f"Failed to update price and quantity on Woocommerce item {item.get('id')} with response: {response}")
                     else:
-                        InventoryModel.objects.filter(user_id=user.user_id, inventory_id=existing_item.id).update(market_item_url=item.get("permalink"))
+                        InventoryModel.objects.filter(user_id=user.user_id, id=existing_item.id).update(market_item_url=item.get("permalink"))
             except Exception as e:
                 try:
                     # If item does not exist, insert new item
@@ -479,15 +479,15 @@ def manually_download_item_from_marketplace_syc_update(userid, access_token):
                         if user.maximum_quantity:
                             if user.maximum_quantity < existing_item.quantity:
                                 item["ebay_quantity"] = user.maximum_quantity
-                                
+
                         response = update_items_quantity_or_price_on_ebay(user.user_id, item.get("ebay_item_id"), existing_item.start_price, item.get("ebay_quantity"), user._id)
                         if "Success" in response:
-                            item_to_save, created = MarketPlaceUpdateLog.objects.update_or_create(user_id=user.user_id, inventory_id=existing_item.id, defaults=dict(market_name="Ebay", vendor_name=existing_item.vendor_name, updated_sku=item.get("ebay_sku"), log_description=f"Updated price to {existing_item.start_price} and quantity to {item.get('ebay_quantity')} from vendor {existing_item.vendor_name}"))
-                            InventoryModel.objects.filter(user_id=user.user_id, inventory_id=existing_item.id).update(market_item_url=item.get("market_item_url"), last_updated=timezone.now())
+                            item_to_save, created = MarketPlaceUpdateLog.objects.update_or_create(user_id=user.user_id, id=existing_item.id, defaults=dict(market_name="Ebay", vendor_name=existing_item.vendor_name, updated_sku=item.get("ebay_sku"), log_description=f"Updated price to {existing_item.start_price} and quantity to {item.get('ebay_quantity')} from vendor {existing_item.vendor_name}"))
+                            InventoryModel.objects.filter(user_id=user.user_id, id=existing_item.id).update(market_item_url=item.get("market_item_url"), last_updated=timezone.now())
                         else:
                             logger.info(f"Failed to update price and quantity on eBay item {item.get('ebay_item_id')} with response: {response}")
                     else:
-                        InventoryModel.objects.filter(user_id=user.user_id, inventory_id=existing_item.id).update(market_item_url=item.get("market_item_url"))
+                        InventoryModel.objects.filter(user_id=user.user_id, id=existing_item.id).update(market_item_url=item.get("market_item_url"))
 
                 except Exception as e:
                     try:
@@ -529,13 +529,13 @@ def manually_download_item_from_marketplace_syc_update(userid, access_token):
                     if existing_item.start_price != item.get("price") or existing_item.quantity != item.get("stock_quantity"):
                         response = update_woocommerce_product_from_background(item.get("id"), existing_item.start_price, existing_item.quantity, userid)
                         if response == "Success":
-                            item_to_save, created = MarketPlaceUpdateLog.objects.update_or_create(user_id=userid, inventory_id=existing_item.id, defaults=dict(market_name="Woocommerce", vendor_name=existing_item.vendor_name, updated_sku=item.get('sku'), log_description=f"Updated price to {existing_item.start_price} and quantity to {existing_item.quantity} from vendor {existing_item.vendor_name}"))
+                            item_to_save, created = MarketPlaceUpdateLog.objects.update_or_create(user_id=userid, id=existing_item.id, defaults=dict(market_name="Woocommerce", vendor_name=existing_item.vendor_name, updated_sku=item.get('sku'), log_description=f"Updated price to {existing_item.start_price} and quantity to {existing_item.quantity} from vendor {existing_item.vendor_name}"))
                             # Update the market url on inventory
-                            InventoryModel.objects.filter(user_id=userid, inventory_id=existing_item.id).update(market_item_url=item.get("permalink"))
+                            InventoryModel.objects.filter(user_id=userid, id=existing_item.id).update(market_item_url=item.get("permalink"))
                         else:
                             logger.info(f"Failed to update price and quantity on Woocommerce item {item.get('id')} with response: {response}")
                     else:
-                        InventoryModel.objects.filter(user_id=userid, inventory_id=existing_item.id).update(market_item_url=item.get("permalink"))
+                        InventoryModel.objects.filter(user_id=userid, id=existing_item.id).update(market_item_url=item.get("permalink"))
             except:
                 try:
                     # If item does not exist, insert new item
